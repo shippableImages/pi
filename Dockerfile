b6342f343322d0217a31a08e17292fbd2c14bb05
John Stockbauer <john@shippable.com>
2015-06-30 17:05:01 -0700

    add gcloud sdk for gcr pulls

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM ubuntu:14.04
 
+RUN echo "================= Adding gcloud binaries ============"
+RUN apt-get update
+RUN apt-get install -yy python-dev
+RUN mkdir -p /opt/gcloud
+ADD google-cloud-sdk /opt/gcloud/google-cloud-sdk
+RUN cd /opt/gcloud/google-cloud-sdk && ./install.sh --usage-reporting=false --bash-completion=true --path-update=true
+ENV PATH $PATH:/opt/gcloud/google-cloud-sdk/bin
+RUN gcloud components update preview
+
 ADD pull.sh /
 
 ENTRYPOINT ["/bin/bash", "/pull.sh"]
--- pull.sh
+++ pull.sh
@@ -19,7 +19,9 @@ docker_path=$(which docker)
 DOCKER_LOGIN=$DOCKER_LOGIN
 DOCKER_PWD=$DOCKER_PWD
 DOCKER_EMAIL=$DOCKER_EMAIL
+GCR_KEY=$GCR_KEY
 PULL_IMAGE_NAME=$PULL_IMAGE_NAME
+PULL_IMAGE_TYPE=$PULL_IMAGE_TYPE
 
 check_env() {
   env_name=$1
@@ -30,17 +32,29 @@ check_env() {
   fi
 }
 
-check_env "DOCKER_LOGIN" "$DOCKER_LOGIN"
-check_env "DOCKER_PWD" "$DOCKER_PWD"
-check_env "DOCKER_EMAIL" "$DOCKER_EMAIL"
+check_env "PULL_IMAGE_TYPE" "$PULL_IMAGE_TYPE"
 check_env "PULL_IMAGE_NAME" "$PULL_IMAGE_NAME"
 
 if [ -z "$docker_path" ]; then
   echo "ERROR: 'docker' command not present'"
   exit 1
 else
-  echo "pulling $PULL_IMAGE_NAME"
-  sudo docker login -u $DOCKER_LOGIN -p $DOCKER_PWD -e $DOCKER_EMAIL
-  sudo docker pull $PULL_IMAGE_NAME
-  sleep infinity
+  if [ $PULL_IMAGE_TYPE == "Docker" ]; then
+    ## must be a docker integration
+    check_env "DOCKER_LOGIN" "$DOCKER_LOGIN"
+    check_env "DOCKER_PWD" "$DOCKER_PWD"
+    check_env "DOCKER_EMAIL" "$DOCKER_EMAIL"
+    sudo docker login -u $DOCKER_LOGIN -p $DOCKER_PWD -e $DOCKER_EMAIL
+    sudo docker pull $PULL_IMAGE_NAME
+    sleep infinity
+  elif [ $PULL_IMAGE_TYPE == "GCR" ]; then
+    ## must be a GCR integration
+    check_env "GCR_KEY" "$GCR_KEY"
+    cd /tmp
+    supress=$(echo $GCR_KEY > key.json)
+    gcloud -q auth activate-service-account --key-file key.json
+    gcloud docker -a
+    sudo docker pull $PULL_IMAGE_NAME
+    sleep infinity
+  fi
 fi
